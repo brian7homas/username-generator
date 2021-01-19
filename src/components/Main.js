3fa07d7d78426eb1867a451b38d26af0e6d355ff
brian <thisnameistak#n11@gmail.com>
2021-01-19 10:50:13 -0700

    fixed hyphen and score position

--- src/components/Main.js
+++ src/components/Main.js
@@ -141,31 +141,30 @@ class Main extends Component {
     
     if(event.target.checked){
       if(hyphen1 && this.state.randWord){
-        hyphen1.innerHTML += `<h1 id="hyphen">${this.state.hyphen}</h1>`
+        hyphen1.innerHTML += `<h1 id="hyphen-1">${this.state.hyphen}</h1>`
       }
       if(hyphen3 && this.state.randNumber){
-        hyphen3.innerHTML += `<h1 id="hyphen">${this.state.hyphen}</h1>`
+        hyphen3.innerHTML += `<h1 id="hyphen-3">${this.state.hyphen}</h1>`
       }
-      hyphen2.innerHTML += `<h1 id="hyphen">${this.state.hyphen}</h1>`
+      hyphen2.innerHTML += `<h1 id="hyphen-2">${this.state.hyphen}</h1>`
       
       
       
     }
     if(!event.target.checked){
       if(hyphen1){
-        var one = document.getElementById("hyphen");
+        var one = document.getElementById("hyphen-1");
         hyphen1.removeChild(one)
       }
+      if(hyphen3){
+        var three = document.getElementById("hyphen-3");
+        hyphen3.removeChild(three)
+      }
       
-      var two = document.getElementById("hyphen");
+      var two = document.getElementById("hyphen-2");
       hyphen2.removeChild(two)
-      var three = document.getElementById("hyphen");
-      hyphen3.removeChild(three)
-      
-      
-      
-      
-      
+      // var three = document.getElementById("hyphen");
+      // hyphen3.removeChild(three)
     }
   }
   
@@ -179,25 +178,29 @@ class Main extends Component {
     var score2 = document.querySelector(".input__container-display > span > div:nth-child(4");
     
     
-                                        
     if(event.target.checked){
       if(score1 && this.state.randWord){
-        score1.innerHTML += `<h1 id="underscore">${this.state.underscore}</h1>`
+        score1.innerHTML += `<h1 id="underscore-1">${this.state.underscore}</h1>`
       }
       if(score3 && this.state.randWord){
-        score3.innerHTML += `<h1 id="underscore">${this.state.underscore}</h1>`
+        score3.innerHTML += `<h1 id="underscore-3">${this.state.underscore}</h1>`
       }
-      score2.innerHTML += `<h1 id="underscore">${this.state.underscore}</h1>`
+      score2.innerHTML += `<h1 id="underscore-2">${this.state.underscore}</h1>`
     }
     if(!event.target.checked){
       if(score1){
-        var underscore1 = document.getElementById("underscore");
+        var underscore1 = document.getElementById("underscore-1");
         score1.removeChild(underscore1)
       }
-      var underscore2 = document.getElementById("underscore");
+      if(score3){
+        var underscore3 = document.getElementById("underscore-3");
+        score3.removeChild(underscore3)
+      }
+      
+      var underscore2 = document.getElementById("underscore-2");
       score2.removeChild(underscore2)
-      var underscore3 = document.getElementById("underscore");
-      score3.removeChild(underscore3)
+      // var underscore3 = document.getElementById("underscore-3");
+      // score3.removeChild(underscore3)
       
     }
   }
