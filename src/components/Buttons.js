133c1e070dce842fd1d8f4bb76b46ee32328bd3f
brian <thisnameistak#n11@gmail.com>
2021-01-24 18:28:03 -0700

    fin

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,10 +6,10 @@ function Buttons({generate, numberClick, clear}) {
   
   return (
     <>
-      <div className="input__container-buttons">
-              <button onClick={generate} type="submit">Get insight based on words used</button>
-              <button onClick={numberClick}>Number</button>
-              <button onClick={clear}>Clear</button>
+      <button className="input__container-buttons__generate" onClick={generate} type="submit">Get information about your username</button>
+      <div className="input__container-buttons">      
+            <button onClick={numberClick}>Number</button>
+            <button onClick={clear}>Clear</button>
       </div>
       
     </>
